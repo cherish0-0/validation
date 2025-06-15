38ae284f387f1ab9097edc40e136d473d103a605
cherish0-0 <jiyu01160@naver.com>
2025-06-15 22:17:33 +0900

    feat: BeanValidation groups 기능 활용을 위해 SaveCheck, UpdateCheck 인터페이스 추가 feat(Item): groups 적용 feat(controller): addForm, editForm 각자 다른 검증 기능 적용

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,20 +11,21 @@ import org.hibernate.validator.constraints.ScriptAssert;
 // @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000 이상이어야 합니다.")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class) // 수정 요구사항 추가
     private Long id;
 
     // 빈 값, null, 공백 허용 안 함 (자동으로 기본 에러 메시지 생성)
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
     // null 허용 안 함, 범위 1000 ~ 1,000,000 (자동으로 기본 에러 메시지 생성)
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
     // null 허용 안 함, 최대 9999
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
