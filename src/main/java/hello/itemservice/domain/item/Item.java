7c7a1564d290195742680aa87235dba6682615df
cherish0-0 <jiyu01160@naver.com>
2025-06-15 21:34:43 +0900

    feat: @ScriptAssert 주석 처리

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -8,7 +8,7 @@ import org.hibernate.validator.constraints.Range;
 import org.hibernate.validator.constraints.ScriptAssert;
 
 @Data
-@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000 이상이어야 합니다.")
+// @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000 이상이어야 합니다.")
 public class Item {
 
     private Long id;
